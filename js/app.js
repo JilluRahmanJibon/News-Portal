8a62969f18b047e85ccd6c97416356da6930df2b
JilluRahmanJibon <jillurahmanjibon@gmail.com>
2022-09-02 22:57:49 +0600

    catagoris added

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const loadNewsPortal = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const response = await fetch(url)
+    const data = response.json()
+    return data
+}
+const displayCatagorisNewsPortal = async () => {
+    const displayCatagoris = await loadNewsPortal()
+
+    // console.log(displayCatagoris);
+    const catagorisContainer = document.getElementById('catagories-container');
+    displayCatagoris.data.news_category.forEach(data => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        
+        <a href="">${data.category_name}</a>
+        `
+        catagorisContainer.appendChild(div)
+
+    })
+}
+displayCatagorisNewsPortal()
